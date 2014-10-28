ecceed068d35d0602deedbf054848e9d9a165a32
Jakob Gaard Andersen <jakobgaardandersen@Jakobs-MacBook-Pro.local>
2014-10-28 10:59:52 +0100

    Edited the jade file, to automatically create the tables with the data

--- database/mongo.js
+++ database/mongo.js
@@ -1,3 +1,15 @@
 /**
  * Created by jakobgaardandersen on 28/10/14.
  */
+var mongoose = require('mongoose');
+
+module.exports.connect = function () {
+    mongoose.connect('mongodb://localhost/northwind');
+    mongoose.connection.once('open', function () {
+        console.log('Connected to db');
+    });
+};
+
+module.exports.close = function () {
+    mongoose.connection.close();
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,43 @@
 /**
  * Created by jakobgaardandersen on 28/10/14.
  */
+var express = require('express');
+var mongo = require('../database/mongo');
+var router = express.Router();
+var models = require('../database/model');
+
+router.get('/', function (req, res) {
+    getOrders(function (err, orders) {
+        if (err) {
+            res.render(error, {
+                message: err.message,
+                error: err
+            });
+        } else {
+            console.log(orders);
+            res.render('orders', {data: orders});
+        }
+    });
+});
+
+function getOrders(callback) {
+    mongo.connect();
+    var orders = [];
+    models.OrderModel.find(function (err, data) {
+        if (err) {
+            callback(err);
+            console.log(err);
+        } else {
+            orders = data;
+        }
+        mongo.close();
+        callback(null, orders);
+    });
+}
+
+router.get('/:id', function(req, res) {
+
+});
+
+module.exports = router;
+
