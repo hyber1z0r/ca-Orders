e13056641bb55ef7afa983d10992f2d802e4d566
Jakob Gaard Andersen <jakobgaardandersen@Jakobs-MacBook-Pro.local>
2014-10-30 13:16:26 +0100

    Added ajax call to delete route, and added some nice toasts

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,32 +1,33 @@
 function goBack() {
     window.history.back()
 }
-$(document).ready(function(){
+$(document).ready(function () {
     $("#BackButton2").click(goBack)
     $("#BackButton").click(goBack);
-    setTimeout("slideit()",2500);
+    setTimeout("slideit()", 2500);
     $('#slider > img#1').fadeIn(300);
     startSlider();
+    addDeleteHandler();
 });
 
-function startSlider(){
+function startSlider() {
     count = $("#slider > img").size();
 
-    loop = setInterval(function(){
+    loop = setInterval(function () {
 
-        if(sliderNext > count){
+        if (sliderNext > count) {
             sliderNext = 1;
             sliderInt = 1;
         }
-    $("#slider > img").fadeOut(300);
-    $("#slider > img#" + sliderNext).fadeIn(300);
+        $("#slider > img").fadeOut(300);
+        $("#slider > img#" + sliderNext).fadeIn(300);
 
         sliderInt = sliderNext;
-        sliderNext = sliderNext +1;
+        sliderNext = sliderNext + 1;
     }, 3000)
 }
 
-$(window).load(function() {
+$(window).load(function () {
     $('.flexslider').flexslider({
         animation: "slide",
         animationLoop: false,
@@ -35,38 +36,37 @@ $(window).load(function() {
     });
 });
 
-var step=1
-function slideit()
-{
-document.images.slide.src=eval(""+step+".jpg")
-if(step<4)
-step++
-else
-step=1
-    }
+var step = 1
+function slideit() {
+    document.images.slide.src = eval("" + step + ".jpg")
+    if (step < 4)
+        step++
+    else
+        step = 1
+}
 
 sliderInt = 1;
 sliderNext = 2;
 
-function prev(){
+function prev() {
     newSlide = sliderInt - 1;
     showSlide(newSlide);
 }
-function next(){
+function next() {
     newSlide = sliderInt + 1;
     showSlide(newSlide);
 }
 
-function stopLoop (){
+function stopLoop() {
     window.clearInterval(loop);
 
 }
 
-function showSlide(Id){
+function showSlide(Id) {
     stopLoop();
-    if(Id > count){
+    if (Id > count) {
         Id = 1;
-        }else if(Id <1){
+    } else if (Id < 1) {
         Id = count;
     }
 
@@ -74,6 +74,60 @@ function showSlide(Id){
     $("#slider > img#" + Id).fadeIn(300);
 
     sliderInt = Id;
-    sliderNext = Id +1;
+    sliderNext = Id + 1;
 }
 
+function addDeleteHandler() {
+    $('#orderdelete').click(function () {
+        // select the id
+        var id = $('#deletebutton').attr('rowid');
+        $.ajax({
+            url: './orders/' + id,
+            type: 'DELETE'
+        }).done(function (data) {
+            if(data.status == 'ok'){
+                // show toast with success
+                toastSuccess();
+                setTimeout('location.reload(true)',2500);
+            }
+        }).fail(function () {
+            toastFailure();
+        })
+    })
+}
+
+function toastSuccess(){
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "positionClass": "toast-bottom-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    toastr.success("Successfully deleted the order", "Success");
+}
+
+function toastFailure(){
+    toastr.error("The order couldn't be deleted, due to some technical error.", "Error");
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "positionClass": "toast-bottom-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+}
