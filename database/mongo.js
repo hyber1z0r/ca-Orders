ed1b950680ba70e4c47247400d67754d3f3d99a9
Jakob Gaard Andersen <jakobgaardandersen@Jakobs-MacBook-Pro.local>
2014-10-30 15:41:07 +0100

    Added hyperlinks in most of pages to lead to other pages

--- database/mongo.js
+++ database/mongo.js
@@ -4,8 +4,8 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = function () {
-    mongoose.connect('mongodb://test:test@ds063769.mongolab.com:63769/orderviewer');
-//    mongoose.connect('mongodb://localhost/northwind');
+//    mongoose.connect('mongodb://test:test@ds063769.mongolab.com:63769/orderviewer');
+    mongoose.connect('mongodb://localhost/northwind');
 
     mongoose.connection.once('open', function () {
         console.log('Connected to db');
